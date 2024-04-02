29cc9e74ed5cb28cdcc6b65c389f476376dcce83
miralcode <johnlloydmiral17@gmail.com>
2024-04-03 01:41:18 +0800

    Final commit

--- _helpers/swagger.js
+++ _helpers/swagger.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
+const swaggyaml = require('yamljs');
 
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = swaggyaml.load('./swagger.yaml');
 
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDocument));
